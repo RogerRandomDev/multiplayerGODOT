dac17f2f3077f16bb387bb1d57e2216771551b16
Roger Grange <rgrang816@west-mec.local>
2022-05-10 18:21:53 -0700

    a

--- multiplayer/node_2d.gd
+++ multiplayer/node_2d.gd
@@ -1,71 +1,91 @@
 extends Node2D
 
 @export_range(10000,60000) var port=25000
-@export var targetIP = "localhost"
+@export var targetIP = "111.123.643"
 @export var server:bool=true
-
+var update_speed=0.1
 var id=0
 
+
+func _ready():
+	var t=Timer.new()
+	add_child(t)
+	t.wait_time=update_speed
+	t.connect('timeout',update_objects)
+	t.start()
+
 func create_server(host:bool):
+#	targetIP=buildIP()
+	targetIP="localhost"
+	
 	var peer=ENetMultiplayerPeer.new()
 	if host:
 		multiplayer.peer_connected.connect(self.load_player)
 		multiplayer.peer_disconnected.connect(self.remove_player)
 		peer.create_server(port,32)
 		
-		print("made server")
 	else:
 		peer.create_client(targetIP,port)
 		
 		
 	multiplayer.set_multiplayer_peer(peer)
-	
-		
+	id =multiplayer.get_unique_id()
+	if host:load_player(1)
+	$HFlowContainer.queue_free()
 var known_peers=[]
 func load_player(id2):
-	id=id2
 	known_peers.push_back(id2)
 	for player in known_peers:
+		if id2==id:continue
 		rpc_id(id2,StringName("player_join"),player)
+		if player==id:continue
 		rpc_id(player,StringName("player_join"),id2)
-	create_player(id2)
+	if server:
+		create_player(id2)
+	
 
 
 
 @rpc(any_peer,reliable)
 func player_join(id2):
+	if get_node_or_null("Players/"+str(id2))!=null:return
 	create_player(id2)
 func create_player(id2):
 	var player=preload("res://player.tscn").instantiate()
 	player.name=str(id2)
-	add_child(player)
+	$Players.add_child(player)
 
 func remove_player(id):
-	get_node(str(id)).queue_free()
+	get_node("Players/"+str(id)).queue_free()
 
 
 func _on_button_2_pressed():
+	server=false
 	create_server(false)
 
 
 func _on_button_pressed():
+	server=true
 	create_server(true)
 
-
+var players={}
 
 @rpc(any_peer,unreliable_ordered)
 func update_peers(peer,data):
-	for child in data:
-		get_node(str(child)).move_to=data[child]
-
-
-func _process(delta):
-	if !multiplayer.is_server():return
-	var player_list={}
-	for child in get_children():
-		if typeof(str2var(child.name))==typeof(int(1)):
-			player_list[str2var(child.name)]=child.position
-	for peer in player_list:
-		if !known_peers.has(peer):continue
-		rpc_id(peer,StringName("update_peers"),peer,player_list)
 	
+	for child in data:
+		var add=get_node_or_null("Players/"+str(child))
+		if add==null:continue
+		
+		add.move_to=data[child][0]
+
+
+func update_objects():
+	if !multiplayer.is_server():return
+	print(players)
+	for peer in players:
+		if !known_peers.has(peer)||peer==id:continue
+		rpc_id(peer,StringName("update_peers"),peer,players)
+
+
+
--- multiplayer/player.gd
+++ multiplayer/player.gd
@@ -1,29 +1,41 @@
-extends Sprite2D
+extends CharacterBody2D
 
 
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var move_to=Vector2.ZERO
-var Speed=64
-func _process(delta):
+func _physics_process(delta):
 	if name!=str(multiplayer.get_unique_id()):
-		position=(move_to-position)*(delta*10)+position
+		position=(move_to-position)*(delta/0.1)+position
 		return
-	var speed=Vector2.ZERO
-	if Input.is_key_pressed(KEY_W):
-		speed.y-=delta
-	if Input.is_key_pressed(KEY_A):
-		speed.x-=delta
-	if Input.is_key_pressed(KEY_S):
-		speed.y+=delta
-	if Input.is_key_pressed(KEY_D):
-		speed.x+=delta
-	position+=speed*Speed
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
 	if !multiplayer.get_peers().has(1):return
-	rpc_id(1,StringName("push_to_server"),position)
+	rpc_id(1,StringName("push_to_server"),name,[position])
 
 
 
 @rpc(any_peer,unreliable_ordered)
-func push_to_server(pos):
-	if !multiplayer.is_server():return
-	if name!=str(multiplayer.get_remote_sender_id()):return
-	move_to=pos
+func push_to_server(peer,pos):
+	var root=get_parent().get_parent()
+	root.players[peer]=pos
+	
