aecb317a031b718c6689e7f44037904f2e34f518
Roger Grange <rgrang816@west-mec.local>
2022-05-12 17:55:38 -0700

    working on lan still

--- multiplayer/node_2d.gd
+++ multiplayer/node_2d.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var port=2049
-var udp_port=2050
+var port=12049
+var udp_port=12050
 var targetIP=""
 var udp=PacketPeerUDP.new()
 var peer=ENetMultiplayerPeer.new()
@@ -13,17 +13,20 @@ var sendPacket=""
 
 var connected=false
 func _ready():
-	sendPacket="MyServer".to_ascii_buffer()
+	
 	setup()
 
 
 func host():
-	
+	var targetIP=IP.get_local_addresses()[7]
 	udp.set_broadcast_enabled(true)
-	peer.create_server(port,4)
+	peer.set_bind_ip(targetIP)
+	peer.create_server(port,16)
 	is_host="hostServer"
+	sendPacket=targetIP.to_ascii_buffer()
 	multiplayer.set_multiplayer_peer(peer)
 	peer.connect("peer_connected",print_peer)
+	
 
 
 func update_connection():
@@ -44,28 +47,26 @@ func _process(delta):
 	call(is_host,delta)
 
 func listenFor(delta):
-	
 	if udp.get_available_packet_count()>0:
-		targetIP=udp.get_packet_ip()
-		print(targetIP)
-		var packet=udp.get_packet().get_string_from_ascii()
 		
+		var packet=udp.get_packet().get_string_from_ascii()
+		print(packet)
+		targetIP=packet
 	
 func hostServer(delta):
 	reCheck-=delta
 	if reCheck>0.0:return
 	reCheck=0.5;
 	udp.set_dest_address('255.255.255.255',udp_port)
+	
 	udp.put_packet(sendPacket)
-
 func asClient(delta):
 	pass
 
 
 func setup() -> void:
 	peer.set_target_peer(ENetMultiplayerPeer.TARGET_PEER_SERVER)
-	udp.bind(udp_port)
-	
+	if udp.bind(udp_port)!=OK:print("failed connection")
 
 
 func print_peer(id):
